cf076152cb966290e226ba3e1d7c41ff58dc4aac
Jhon leo Ronquillo <jhle.ronquillo.au@phinmaed.com>
2026-09-23 15:17:05 +0800

    first commit
    Implement a student activity score system that calculates and displays student averages and statuses based on their activity scores.

--- LEO_P2.py
+++ LEO_P2.py
@@ -1,1 +1,33 @@
+print("STUDENT ACTIVITY SCORE SYSTEM") 
 
+Students = int(input("\nHow many students? "))
+
+def Students_info ():
+    S_1 = str(input("Enter your name:"))
+    A = float(input("Activity 1:"))
+    B = float(input("Activity 2:"))
+    C = float(input("Activity 3:"))
+
+    return S_1, (A + B + C)/3
+
+for i in range (Students):
+    print("\nSTUDENT:", i + 1)
+    S_1, Average = Students_info()
+
+    if Average >= 90:
+        Status = "Excellent"
+    elif Average >= 80:
+        Status = "Very Good"
+    elif Average >= 75:
+        Status = "Passed"
+    else:
+        Status = "Failed"
+
+
+
+    print("STUDENT REPORT")
+    print("\nNAME: ", S_1)
+    print("Average: ",round(Average, 2))
+    print("Status: ", Status)
+
+    
